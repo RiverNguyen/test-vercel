177673f457a181ea7cd6c734c59f68ee3010f307
RiverNguyen <ngiang1309@gmail.com>
2024-07-31 14:44:57 +0700

    update worker model

--- models/worker.js
+++ models/worker.js
@@ -1,32 +1,44 @@
 import mongoose from "mongoose";
 
 const workerSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        age: {
-            type: Number,
-            required: true,
-        },
-        address: {
-            type: String,
-            required: true,
-        },
-        phone: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-        },
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
-        versionKey: false,
-    }
+    age: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    province: {
+      type: String,
+      required: true,
+    },
+    district: {
+      type: String,
+      required: true,
+    },
+    ward: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
 );
 
 export const Worker = mongoose.model("Worker", workerSchema);
